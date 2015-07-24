fa9a004c69d832f8df86f9b822d0bf441049eacc
Noel Martin Llevares <noelmartin@gmail.com>
2015-07-24 22:12:33 +0800

    make the code more pythonic

--- kroman/__init__.py
+++ kroman/__init__.py
@@ -1,95 +1,92 @@
-head_jamos = {
-    0: "g",
-    1: "gg",
-    2: "n",
-    3: "d",
-    4: "dd",
-    5: "r",
-    6: "m",
-    7: "b",
-    8: "bb",
-    9: "s",
-    10: "ss",
-    11: "",
-    12: "j",
-    13: "jj",
-    14: "c",
-    15: "k",
-    16: "t",
-    17: "p",
-    18: "h"
-}
-
-body_jamos = {
-    0: "a",
-    1: "ae",
-    2: "ya",
-    3: "yae",
-    4: "eo",
-    5: "e",
-    6: "yeo",
-    7: "ye",
-    8: "o",
-    9: "wa",
-    10: "wae",
-    11: "oe",
-    12: "yo",
-    13: "u",
-    14: "weo",
-    15: "we",
-    16: "wi",
-    17: "yu",
-    18: "eu",
-    19: "eui",
-    20: "i"
-}
-
-tail_jamos = {
-    0: "",
-    1: "g",
-    2: "gg",
-    3: "gs",
-    4: "n",
-    5: "nj",
-    6: "nh",
-    7: "d",
-    8: "r",
-    9: "rk",
-    10: "rm",
-    11: "rb",
-    12: "rs",
-    13: "rt",
-    14: "rp",
-    15: "rh",
-    16: "m",
-    17: "b",
-    18: "bs",
-    19: "s",
-    20: "ss",
-    21: "ng",
-    22: "j",
-    23: "c",
-    24: "k",
-    25: "t",
-    26: "p",
-    27: "h"
-}
-
 import sys
 import math
+
+head_jamos = [
+    'g',
+    'gg',
+    'n',
+    'd',
+    'dd',
+    'r',
+    'm',
+    'b',
+    'bb',
+    's',
+    'ss',
+    '',
+    'j',
+    'jj',
+    'c',
+    'k',
+    't',
+    'p',
+    'h'
+]
+
+body_jamos = [
+    'a',
+    'ae',
+    'ya',
+    'yae',
+    'eo',
+    'e',
+    'yeo',
+    'ye',
+    'o',
+    'wa',
+    'wae',
+    'oe',
+    'yo',
+    'u',
+    'weo',
+    'we',
+    'wi',
+    'yu',
+    'eu',
+    'eui',
+    'i'
+]
+
+tail_jamos = [
+    '',
+    'g',
+    'gg',
+    'gs',
+    'n',
+    'nj',
+    'nh',
+    'd',
+    'r',
+    'rk',
+    'rm',
+    'rb',
+    'rs',
+    'rt',
+    'rp',
+    'rh',
+    'm',
+    'b',
+    'bs',
+    's',
+    'ss',
+    'ng',
+    'j',
+    'c',
+    'k',
+    't',
+    'p',
+    'h'
+]
 
 
 def parse(text):
     if sys.version_info[0] == 2:
-        text = unicode(text, "utf-8")
-    # else:
-        #text = str(text, "utf-8")
-    retval = u""
+        text = unicode(text, 'utf-8')
+    retval = u''
     ga = 0xac00
     hih = 0xd7a3
     interval_head = 588
     interval_body = 28
-    interval_tail = 1
     last_char_is_hangul = False
 
     for c in text:
@@ -100,7 +97,7 @@ def parse(text):
             body = int(math.floor(headl / interval_body))
             tail = int(math.floor(headl % interval_body))
             if last_char_is_hangul:
-                retval += "-"
+                retval += '-'
             retval += head_jamos[head]
             retval += body_jamos[body]
             retval += tail_jamos[tail]
